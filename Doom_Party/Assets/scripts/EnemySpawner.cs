f278559fc3f1176c98fe011f02ac70efce02edca
angel diaz <adiaz004@yahoo.com>
2016-02-12 15:12:22 -0700

    updated implemenatation on the enemy spawner.
    renamed the scripts, each enemy spanwner represents one instance of the spawner.
    aded an AI script to the enemy, it's just a starting point for the enemy AI.

---
+++ Doom_Party/Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Enemy Spawner.
+/// 	Spawns everytime N seconds at defined spawnPoint.
+/// 	When health reaches zero it will destroy itself.
+/// 
+/// Angel Diaz
+/// </summary>
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform spawnPoint;
+	public int health = 100;
+    
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	/// <summary>
+	/// Update function, called every frame.
+	/// </summary>
+	void Update () {
+		if (health == 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	/// <summary>
+	/// Spawn an instance of the enemy.
+	/// </summary>
+    void Spawn ()
+    {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }
+
+	/// <summary>
+	/// Takes the damage.
+	/// </summary>
+	/// <param name="damage">Damage.</param>
+	public void TakeDamage (int damage) {
+		health = health - damage;
+		if (health < 0) {
+			health = 0;
+		}
+	}
+}
