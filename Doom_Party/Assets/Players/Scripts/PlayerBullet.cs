20e517754397001e07bc9bbb7839022532d124aa
S. Alex Bradt <jai7breaker@gmail.com>
2016-03-25 01:27:21 -0700

    Creating classes to handle all four players' controls without all that redundant code. Still in progress.

---
+++ Doom_Party/Assets/Players/Scripts/PlayerBullet.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerBullet : MonoBehaviour {
+	
+	[SerializeField] private GameObject BulletHit;
+	[SerializeField] private GameObject Blood;
+	[SerializeField] private Timer Lifespan;
+	private float speedX;
+	private float speedY;
+	private float speed;
+	private int deleteTime;
+	private float angle;
+	
+	void Start() {
+		speedX = 0;
+		speedY = 0;
+		speed = 0.5f; //bullet speed
+		Lifespan.Reset();
+		
+		if (MyGlobalController4.SharedInstance.Mode == 0) {
+			angle = MyGlobalController4.SharedInstance.JoyAngle + Random.Range (-0.06f, 0.06f);
+		} else {
+			if (MyGlobalController4.SharedInstance.Mode == 2) { //Sprint
+				angle = MyGlobalController4.SharedInstance.JoyAngle + Random.Range (-0.2f, 0.2f);
+			} else { // Crouch
+				angle = MyGlobalController4.SharedInstance.JoyAngle;
+			}
+		}
+		
+		//starting distance from the player offset by slight angle
+		speedX = Mathf.Sin(angle+0.13f);
+		speedY = Mathf.Cos(angle+0.13f);
+		transform.position = new Vector3 (transform.position.x + speedX*0.65f-5000.5f, transform.position.y + speedY*0.65f, 0);
+		
+		//Set velocity based on angle
+		speedX = Mathf.Sin(angle);
+		speedY = Mathf.Cos(angle);
+		transform.localEulerAngles = new Vector3 (0,0,angle/Mathf.PI*-180);
+		
+		transform.localScale = new Vector3 (1, 3, 1);
+		
+		// Adjust the speed of the bullets
+		speedX = speedX * speed;
+		speedY = speedY * speed;
+		
+	}
+	
+	
+	void FixedUpdate() {
+		
+		transform.position = new Vector3 (transform.position.x + speedX, transform.position.y + speedY, 0);
+		
+		if (Lifespan.complete)
+			Destroy(gameObject);
+		
+	}
+	
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Wall") {
+			Instantiate (BulletHit, new Vector3 (transform.position.x+speedX/2.0f, transform.position.y+speedY/2.0f,0), Quaternion.identity);
+			Destroy(gameObject);
+		}
+		if (col.gameObject.tag == "Player") {
+			Instantiate (Blood, new Vector3 (transform.position.x+speedX/2.0f, transform.position.y+speedY/2.0f,0), Quaternion.identity);
+			Destroy(gameObject);
+		}
+		if (col.gameObject.tag == "Enemy") {
+			Instantiate (Blood, new Vector3 (transform.position.x+speedX/2.0f, transform.position.y+speedY/2.0f,0), Quaternion.identity);
+			Destroy(gameObject);
+		}
+	}
+	
+	
+}
---
+++ Doom_Party/Assets/Players/Scripts/PlayerControls.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+	
+	[SerializeField] private GameObject GunSound;
+	[SerializeField] private GameObject BulletPrefab;
+	[SerializeField] private Rigidbody2D player;
+	[SerializeField] private Timer ShootingTimer;
+	// The Timer class is like what you were doing with the Delay variable, except it relies on actual time instead of Update calls. And it's easier to manipulate. -Alex.
+	[SerializeField] private string CrouchButton, SprintButton;
+	[SerializeField] private string HorizontalJoystick, VerticalJoystick;
+	[SerializeField] private string FireButton;
+	[SerializeField] private float Speed = 0.05f;
+	[SerializeField] private int Health = 100;
+	// The [SerializeField] tag exposes a field in the inspector without making it public to other classes. -Alex
+	
+	private bool ReadyFire { get { return ShootingTimer.complete; } }
+//	private float Jangle = 0; // Joystick Angle
+//	private float JoyX = 0;   // Joystick X Axis
+//	private float JoyY = 0;   // Joystick Y Axis
+//	private bool Crouching = false;
+//	private bool Sprinting = false;
+	
+	//private int Score = 0;
+	
+	void Start () {
+		ShootingTimer.Reset();
+		Physics2D.IgnoreLayerCollision(8, 9);
+	}
+	
+	void FixedUpdate () {
+		bool Crouching = false, Sprinting = false;
+		// Crouch and Sprint Buttons
+		if (Input.GetKey (CrouchButton)) { //change c to right mouse click
+			Crouching = true; Sprinting = false; //Crouch Button
+		} else {
+			if (Input.GetKey (SprintButton)) {
+				Crouching = false; Sprinting = true; //Sprint Button
+			} //else {
+				//Crouching = false; Sprinting = false; //default
+			//}
+		}
+		
+		// Get The Players Input
+		float JoyX = Input.GetAxis (HorizontalJoystick);
+		float JoyY = Input.GetAxis (VerticalJoystick);
+		float Jangle = Mathf.Atan2 (JoyX, JoyY);
+		// I'm gonna try putting these in a local variable, since you don't actually reference them outside of this method. -Alex
+		
+		// Move The Player
+		if (Crouching == false && Sprinting == false)
+			transform.position = transform.position + new Vector3(1, 1, 0) * JoyY * Speed;
+		else if (Sprinting == true)
+			transform.position = transform.position + new Vector3(1, 1, 0) * JoyY * Speed * 2.0f;
+			// You know you don't need brackets around a single statement, right? -Alex
+		
+		
+		// Rotate the player
+		transform.localEulerAngles = new Vector3 (0,0,Jangle/Mathf.PI*-180);
+		// Euler angles? That's weird, but I guess it works. -Alex
+		
+		// Weapon is ready to fire
+		if (ReadyFire) {
+			//if (Input.GetKey ("joystick 4 button 0")){
+			if (Input.GetAxis (FireButton) >= 0.5f){
+				Instantiate (BulletPrefab, new Vector3 (transform.position.x + 5000.5f, transform.position.y, 0), Quaternion.identity);
+				Instantiate (GunSound, new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
+				ShootingTimer.Reset();
+			}
+			
+			
+		} //ready to fire
+		
+	} // FixedUpdate
+	
+	//Test Damage
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		/*
+		if (col.gameObject.tag == "Bullet") {
+			P4Health = P4Health - 1;
+			MyGlobalController4.SharedInstance.P4Health = P4Health;
+			//print (P4Health); // display the players numeric health amount
+			if (P4Health <= 0) {
+				//Instantiate (PlayerDeath, new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
+				Destroy(gameObject);
+			}
+		}
+		*/
+	}	
+}
