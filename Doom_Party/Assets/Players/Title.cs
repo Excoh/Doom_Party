686451ea5004a778777353b002e69652bc8874ab
alexgryan <alexryan1995@gmail.com>
2016-04-08 19:52:19 -0700

    Title_Screen
    -title screen put in.
    -build settings adjusted to work with Joey's level

---
+++ Doom_Party/Assets/Players/Title.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKey) {
+			Application.LoadLevel(1); // the scene to be loaded
+		}
+	
+	}
+		
+
+}
+	
+
