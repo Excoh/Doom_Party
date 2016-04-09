2b8d91640cdb229baf4caf26249b7594082e97c6
alexgryan <alexryan1995@gmail.com>
2016-04-08 21:43:07 -0700

    Enemy Spawner Fix
    -Enemy Spawners do not activate until a Player has entered a room now.

--- Doom_Party/Assets/scripts/DoorEntrance.cs
+++ Doom_Party/Assets/scripts/DoorEntrance.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class DoorEntrance : MonoBehaviour {
 
-    public GameObject[] activationArray;
+    public GameObject[] spawnerActivationArray;
+    public GameObject[] doorActivationArray;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag.Equals("Player"))
         {
-			for (int i = 0; i < activationArray.Length; i++)
+            for (int i = 0; i < spawnerActivationArray.Length; i++)
+            {
+                spawnerActivationArray[i].SetActive(true);
+            }
+            for (int i = 0; i < doorActivationArray.Length; i++)
 			{
-				activationArray[i].SetActive(true);
+				doorActivationArray[i].SetActive(true);
 			}
             Destroy(gameObject);
         }
